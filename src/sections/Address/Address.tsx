fe15739e9c2cc37ef4e93bb90efaff6897fe7018
Polina Fan <fanjeewon@yandex.ru>
2026-02-26 19:01:38 +0900

    Компонент с картой

--- src/sections/Address/Address.tsx
+++ src/sections/Address/Address.tsx
@@ -1,7 +1,11 @@
+import { CONTACTS } from "../../shared/config/contacts";
+import Map from "../../shared/ui/Map/Map";
+
 export default function Address() {
   return (
     <section className="address">
       <h2>Адреса</h2>
+      <Map src={CONTACTS.mapUrl} />
     </section>
   )
 }
