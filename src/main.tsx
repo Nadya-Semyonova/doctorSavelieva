74e61c026d87a2f9e1313640c26ca399b8a8636f
Nadya-Semyonova <gerey05@gmail.com>
2026-05-19 14:29:23 +0200

    адаптив для хедера и хиро

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import "./styles/global.css";
 import "./styles/variables.css";
 import App from "./app/App";
 
-// Инициализация EmailJS - замените на свой PUBLIC KEY
-// emailjs.init('YOUR_PUBLIC_KEY');
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
