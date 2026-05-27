3c7af0c490bc84fbc9394e4c197d1a2b1d477045
Nadya-Semyonova <gerey05@gmail.com>
2026-05-27 21:02:32 +0200

    заменено фото

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import doctorImage from "../../assets/images/Gallery/Photo/IMG_9382.png";
+import doctorImage from "../../assets/images/Gallery/Photo/IMG_9916рт.jpg";
 import Wave from "../../shared/ui/Waves/Wave";
 import styles from "./Hero.module.css";
 
@@ -10,13 +10,13 @@ export default function Hero() {
           <div className={styles.textBlock}>
             <h1>Ревматолог</h1>
 
-            <p>врач доказательной медицины с опытом работы 16 лет</p>
+            <p className={styles.text}>врач доказательной медицины с опытом работы 16 лет</p>
           </div>
 
           <div className={styles.textBlock}>
             <h1>Савельева Юлия Олеговна</h1>
 
-            <p>
+            <p className={styles.text}>
               Онлайн консультации по всему миру <br />
               Очный приём в Калининграде
             </p>
